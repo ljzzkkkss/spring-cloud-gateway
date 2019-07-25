d0fef6c3c605c32ec220d8bcbd715bee7193bd59
ljzzkkkss <793958447@qq.com>
2019-07-26 00:00:04 +0800

    全局filter和filter工厂

--- src/main/java/com/example/springcloudgateway/SpringCloudGatewayApplication.java
+++ src/main/java/com/example/springcloudgateway/SpringCloudGatewayApplication.java
@@ -1,7 +1,10 @@
 package com.example.springcloudgateway;
 
+import com.example.springcloudgateway.filter.TokenFilter;
+import com.example.springcloudgateway.filter.factory.RequestTimeGatewayFilterFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
@@ -9,5 +12,15 @@ public class SpringCloudGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudGatewayApplication.class, args);
     }
+
+    @Bean
+    public RequestTimeGatewayFilterFactory elapsedGatewayFilterFactory() {
+        return new RequestTimeGatewayFilterFactory();
+    }
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+
 
 }
